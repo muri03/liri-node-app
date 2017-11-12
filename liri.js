1b8d7d1016ea6e58aed817e669a521abd349981e
Muriel <muriel.mangubat@gmail.com>
2017-11-12 15:09:14 -0600

    recreated files

---
+++ liri.js
@@ -0,0 +1,24 @@
+var inquirer = require("inquirer");
+inquirer.prompt([{
+      type: "list",
+      message: "Which Pokemon do you choose?",
+      choices: ["Bulbasaur", "Squirtle", "Charmander"],
+      name: "pokemon"
+    },
+    {
+      type: "confirm",
+      message: "Are you sure:",
+      name: "confirm",
+      default: true
+    }
+  ])
+    .then(function(inquirerResponse) {
+    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+    if (inquirerResponse.confirm) {
+      console.log("\nWelcome " + inquirerResponse.username);
+      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+    }
+    else {
+      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+    }
+  });
