6a7cd977ee16156fe904fe740a0c76dfd1b5f9c3
Muriel <muriel.mangubat@gmail.com>
2017-11-13 06:11:12 -0600

    twitter working

--- liri.js
+++ liri.js
@@ -1,45 +1,74 @@
-var inquirer = require("inquirer");
-inquirer.prompt([{
-      type: "list",
-      message: "Which Pokemon do you choose?",
-      choices: ["Bulbasaur", "Squirtle", "Charmander"],
-      name: "pokemon"
-    },
-    {
-      type: "confirm",
-      message: "Are you sure:",
-      name: "confirm",
-      default: true
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var fs = require('fs');
+var key = require("./keys.js");
+var client = new Twitter(key);
+
+// For twitter get
+// var twitterKeys = require("keys.js");
+var params = {screen_name: 'm2nucamp'};
+
+var op = process.argv[2];
+
+var input = process.argv[3];
+
+var tweets;
+
+if (op === "my-tweets") {
+	myTweets();
+} else if (op === "spotify-this-song"){
+	spot(input);
+} else if (op === "movie-this") {
+	req();
+} else if (op === "do-what-it-says"){
+	doIt();
+} 
+
+
+
+ // Used to gather last 20 tweets
+function myTweets() {
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    var max = 20;
+    for (i=0; i<tweets.length; i++) {
+      console.log(tweets[i].text);
+      if (i> max) { 
+        break;
+      }
     }
-  ])
-    .then(function(inquirerResponse) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-    if (inquirerResponse.confirm) {
-      console.log("\nWelcome " + inquirerResponse.username);
-      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+  } else if (error) {
+    console.log(error);
     }
-    else {
-      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+  });
+}
+
+
+function spot (input) {
+  spotify.search({ type: 'track', query: input }, function(err, data) {
+    if (err) {
+        // Upon error, call spotErr function
+        spotErr();
+    } else if (!err) {
+      console.log("Song: " + input);
+      console.log("Artist: " + JSON.stringify(data.tracks.items[0].album.artists[0].name, null, 2));
+      console.log("Preview Link: " + JSON.stringify(data.tracks.items[0].album.artists[0].external_urls.spotify, null, 2));
+      console.log("Album: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+    }
+  });
+}
+
+// Function called if error is returned during spot function. Defaults to song 'The Sing' by ace of base 
+function spotErr() {
+  spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+    if ( err ) {
+      console.log('Error occurred: ' + err);
+    } else if (!err) {
+      console.log("Song: The Sign");
+      console.log("Artist: " + JSON.stringify(data.tracks.items[3].album.artists[0].name, null, 2));
+      console.log("Preview Link: " + JSON.stringify(data.tracks.items[3].album.artists[0].external_urls.spotify, null, 2));
+      console.log("Album: " + JSON.stringify(data.tracks.items[3].album.name, null, 2));
     }
   });
 
-
-    var Twit = require('twit');
-
-var config = require('./keys');
-
-var T = new Twit(config);
-
-var params = {
-        q: 'm2nucamp',
-        count: 20
-    }
-
-    T.get('search/tweets', params, gotData);
-
-    function gotData(err, data, response) {
-        var tweets = data.statuses;
-        for (var i = 0; i < tweets.length; i++){
-        console.log('\n > ' + tweets[i].created_at + ': ' + tweets[i].text);
-    }
-    };
+}
